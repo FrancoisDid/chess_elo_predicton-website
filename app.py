6644223229eb0d2b5337c40b6fe361441481753c
Francois Didden <didden.francois@gmail.com>
2024-12-02 10:50:20 +0100

    test api

--- app.py
+++ app.py
@@ -1,9 +1,4 @@
 
-st.markdown('''
-Remember that there are several ways to output content into your web page...
-
-Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
-''')
 
 
 
@@ -22,6 +17,11 @@ from geopy.extra.rate_limiter import RateLimiter
 '''
 # Taxifare calculator
 '''
+st.markdown('''
+Remember that there are several ways to output content into your web page...
+
+Either as with the title by just creating a string (or an f-string). Or as with this paragraph using the `st.` functions
+''')
 
 
 st.balloons()
