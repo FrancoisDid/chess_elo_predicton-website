89d69e156aad01b175f63af1e7b08fb33dbdaabe
jules saint-andre <jules.saint-andre@edhec.com>
2024-12-05 11:38:39 +0100

    change layout web

--- app.py
+++ app.py
@@ -160,7 +160,7 @@ def analysis_page():
 
         with col1:
             if hasattr(st.session_state, 'board_image'):
-                st.image(st.session_state.board_image,width=700)
+                st.image(st.session_state.board_image,width=550)
 
                 # Custom CSS for the smaller buttons
                 st.markdown("""
